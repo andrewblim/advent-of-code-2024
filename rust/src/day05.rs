4f64bc0bca37718122b3ee2ed0ea444964f556dc
Andrew Lim <andrew.b.lim@gmail.com>
2024-12-21 11:54:25 -0500

    Day05 problem 2

--- rust/src/day05.rs
+++ rust/src/day05.rs
@@ -15,9 +15,43 @@ fn read_data() -> String {
 }
 
 fn problem1_str(data: String) -> i64 {
-    let (rules_text, updates_text) = data.trim().split_once("\n\n").unwrap();
+    let (rules_text, updates_text) = data
+        .trim()
+        .split_once("\n\n")
+        .unwrap();
+    let rules = parse_rules(rules_text);
+    let updates = updates_text
+        .split("\n")
+        .map(|line| {
+            line.split(",").map(|x| (*x).parse::<i64>().unwrap()).collect::<Vec<i64>>()
+        });
+    
+    updates
+        .filter(|xs| correctly_ordered(xs, &rules))
+        .map(|xs| xs[xs.len() / 2])
+        .sum()
+}
 
-    let rules = rules_text
+fn problem2_str(data: String) -> i64 {
+    let (rules_text, updates_text) = data
+        .trim()
+        .split_once("\n\n")
+        .unwrap();
+    let rules = parse_rules(rules_text);
+    let updates = updates_text
+        .split("\n")
+        .map(|line| {
+            line.split(",").map(|x| (*x).parse::<i64>().unwrap()).collect::<Vec<i64>>()
+        });
+    
+    updates
+        .filter(|xs| !correctly_ordered(xs, &rules))
+        .map(|xs| middle_from_ordered(&xs, &rules))
+        .sum()
+}
+
+fn parse_rules(rules_text: &str) -> HashMap<i64, HashSet<i64>> {
+    rules_text
         .split("\n")
         .map(|line| {
             let (earlier, later) = line.split_once("|").unwrap();
@@ -26,33 +60,39 @@ fn problem1_str(data: String) -> i64 {
         .fold(HashMap::new(), |mut acc: HashMap<i64, HashSet<i64>>, (earlier, later)| {
             acc.entry(later).or_default().insert(earlier);
             acc
-        });
-
-    let updates = updates_text
-        .split("\n")
-        .map(|line| {
-            line.split(",").map(|x| (*x).parse::<i64>().unwrap()).collect::<Vec<i64>>()
-        });
-    
-    updates
-        .filter(|xs| {
-            let mut disallowed: HashSet<i64> = HashSet::new();
-            for x in xs {
-                if disallowed.contains(&x) {
-                    return false;
-                }
-                if rules.contains_key(&x) {
-                    disallowed.extend(rules[&x].iter());
-                }
-            }
-            true
         })
-        .map(|xs| xs[xs.len() / 2])
-        .sum()
 }
 
-fn problem2_str(data: String) -> u64 {
-    unimplemented!();
+fn correctly_ordered(xs: &Vec<i64>, rules: &HashMap<i64, HashSet<i64>>) -> bool {
+    let mut disallowed: HashSet<i64> = HashSet::new();
+    for x in xs {
+        if disallowed.contains(&x) {
+            return false;
+        }
+        if rules.contains_key(&x) {
+            disallowed.extend(rules[&x].iter());
+        }
+    }
+    true
+}
+
+fn middle_from_ordered(xs: &Vec<i64>, rules: &HashMap<i64, HashSet<i64>>) -> i64 {
+    let mut unplaced: HashSet<i64> = HashSet::from_iter(xs.iter().cloned());
+    let mut placed: Vec<i64> = Vec::new();
+    while placed.len() < xs.len() / 2 + 1 {
+        let next = unplaced.iter().find(|&&x| {
+            if let Some(predecessors) = rules.get(&x) {
+                predecessors.is_disjoint(&unplaced)
+            } else {
+                true
+            }
+        }).unwrap();
+        placed.push(*next);
+        let mut next_unplaced = HashSet::clone(&unplaced);
+        next_unplaced.remove(next);
+        unplaced = next_unplaced;
+    }
+    placed[xs.len() / 2]
 }
 
 #[cfg(test)]
@@ -101,6 +141,6 @@ mod tests {
 
     #[rstest]
     fn problem2_test(input1: String) {
-        assert_eq!(problem2_str(input1), 9);
+        assert_eq!(problem2_str(input1), 123);
     }
 }
