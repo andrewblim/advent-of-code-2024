f422b12aed38f477c95a8a610db6847d10eb57ed
Andrew Lim <andrew.b.lim@gmail.com>
2025-01-02 17:06:53 -0500

    Day12 problem 2

--- rust/src/day12.rs
+++ rust/src/day12.rs
@@ -1,4 +1,6 @@
-use std::{collections::{HashMap, HashSet}, fs};
+use std::{collections::{HashMap, HashSet}, fs, thread::current};
+
+use itertools::Itertools;
 
 pub fn problem1() {
     println!("{}", problem1_str(read_data()));
@@ -24,7 +26,13 @@ fn problem1_str(data: String) -> usize {
 }
 
 fn problem2_str(data: String) -> usize {
-    unimplemented!();
+    let board = parse_board(&data);
+    let shapes = find_shapes(&board);
+    let mut price = 0;
+    for shape in shapes {
+        price += score_shape2(&shape);
+    }
+    price
 }
 
 fn parse_board(data: &str) -> HashMap<(i64, i64), char> {
@@ -95,6 +103,51 @@ fn score_shape(shape: &HashSet<(i64, i64)>) -> usize {
     area * perimeter
 }
 
+fn score_shape2(shape: &HashSet<(i64, i64)>) -> usize {
+    let area = shape.len();
+    let mut n_sides = 0;
+
+    let mut shape_by_row: Vec<&(i64, i64)> = Vec::from_iter(shape.into_iter());
+    shape_by_row.sort();
+
+    let mut current_sides = HashSet::new();
+    for (r, chunk) in &shape_by_row.into_iter().chunk_by(|(r, _)| *r) {
+        let mut next_sides = HashSet::new();
+        for &(_, c) in chunk {
+            for adj_c in [c - 1, c + 1] {
+                if !shape.contains(&(r, adj_c)) {
+                    next_sides.insert((c, adj_c));
+                    if !current_sides.contains(&(c, adj_c)) {
+                        n_sides += 1;
+                    }
+                }
+            }
+        }
+        current_sides = next_sides;
+    }
+
+    let mut shape_by_col: Vec<&(i64, i64)> = Vec::from_iter(shape.into_iter());
+    shape_by_col.sort_by_key(|(r, c)| (c, r));
+
+    let mut current_sides = HashSet::new();
+    for (c, chunk) in &shape_by_col.into_iter().chunk_by(|(_, c)| *c) {
+        let mut next_sides = HashSet::new();
+        for &(r, _) in chunk {
+            for adj_r in [r - 1, r + 1] {
+                if !shape.contains(&(adj_r, c)) {
+                    next_sides.insert((r, adj_r));
+                    if !current_sides.contains(&(r, adj_r)) {
+                        n_sides += 1;
+                    }
+                }
+            }
+        }
+        current_sides = next_sides;
+    }
+
+    area * n_sides
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -137,6 +190,29 @@ MMMISSJEEE
         ")
     }
 
+    #[fixture]
+    fn input4() -> String {
+        String::from("\
+EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE
+        ")
+    }
+
+    #[fixture]
+    fn input5() -> String {
+        String::from("\
+AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA
+        ")
+    }
+
     #[rstest]
     fn problem1_test(input1: String, input2: String, input3: String) {
         assert_eq!(problem1_str(input1), 140);
@@ -144,9 +220,12 @@ MMMISSJEEE
         assert_eq!(problem1_str(input3), 1930);
     }
 
-    #[ignore]
     #[rstest]
-    fn problem2_test(input1: String) {
-        unimplemented!();
+    fn problem2_test(input1: String, input2: String, input3: String, input4: String, input5: String) {
+        assert_eq!(problem2_str(input1), 80);
+        assert_eq!(problem2_str(input2), 436);
+        assert_eq!(problem2_str(input3), 1206);
+        assert_eq!(problem2_str(input4), 236);
+        assert_eq!(problem2_str(input5), 368);
     }
 }
